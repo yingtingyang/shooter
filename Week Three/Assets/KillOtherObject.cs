41dbf472f9f75e273e0276276e9f4906d0f49c1d
yingtingyang <yangy389@newschool.edu>
2016-09-13 17:22:51 -0400

    initial commit

---
+++ Week Three/Assets/KillOtherObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillOtherObject : MonoBehaviour {
+	int lives = 30;
+
+	// Use this for initialization
+	void OnTriggerExit( otherObj ) {
+		otherObj.transform.position =KillOtherObject.position + Vector3.up * 40;
+		otherObj.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero
+		//Destroy otherObj;
+		Debug.Log ("Things moving out of the box.");
+	}
